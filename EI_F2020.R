620cd4a83482e4a6a1b04821745bdfe4e3fd8fce
arthur <notkingarthur@gmail.com>
2021-03-14 17:40:16 -0400

    complete dataset joins
    some graphs

--- EI_F2020.R
+++ EI_F2020.R
@@ -1,1 +1,28 @@
-#test
+# EI Streamdata Project 
+# cheunga
+
+#packages
+library(ggplot2)
+library(tidyverse)
+library(installr)
+library(usethis)
+
+
+#git setup
+# use_git_config(user.name = "#USERNAME", 
+#                user.email = "#EMAIL")
+#
+
+#read-ins
+tempdata <-
+  list.files(pattern = "*.csv") %>% 
+  map_dfr(~read_csv(.) %>% 
+            mutate(Sensor = .x)) %>%
+  mutate_at("Sensor", str_trunc, width = 10, side ='right', ellipsis = '') %>%
+  mutate_at("Sensor", str_replace_all, pattern = ' ', replacement = '')
+
+#main stem data -- site location : "MS"
+#Temp vs Distance (Site locations file)
+
+#example plots
+#easily changeable dates for plot maker
