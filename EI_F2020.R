0c9bb49da92bc5a26b6188e3aea24db7fe382e31
JenniferOchs <jennifero@vt.edu>
2021-03-29 10:33:29 -0400

    3/29 at 10:30am

--- EI_F2020.R
+++ EI_F2020.R
@@ -67,24 +67,165 @@ ggplot(aes(x = date,
            y = , ))
 
 
-#attempted graph of 
-tempdata %>% 
+#multiple dates
+days_to_plot <- mdy_hms(c("01-10-2021 12:00:00", "12-10-2020 12:00:00", "11-10-2020 12:00:00",
+                          "10-10-2020 12:00:00", "09-10-2020 12:00:00", "08-10-2020 12:00:00"))
+
+days_graph <- tempdata_filtered %>% 
+  filter(DateTime %in% days_to_plot) %>%
+  ggplot(aes(factor(Name, levels = c("MS1", "MS2", "MS3", "MS4", "MS5", "MS6", "MS7",
+                                     "MS8", "MS9", "MS10", "MS11", "MS12",
+                                     "MS13", "MS14", "MS15", "MS16", "MS17", "MS18", 
+                                     "MS19", "MS20", "MS21")), TempCor_F, 
+             color = as.factor(DateTime), group = as.factor(DateTime))) + 
+  geom_point()+
+  geom_line()+
+  labs(title = "Water Temp for Each Sensor on the 10th of Each Month",
+       x = "Sensor Name",
+       y = "Temperature (F)")
+
+days_graph
+
+#graph by heat
+days_heat_graph <- tempdata_filtered %>% 
+  filter(DateTime %in% days_to_plot) %>%
+  ggplot(aes(x = factor(Name, levels = c("MS1", "MS2", "MS3", "MS4", "MS5", "MS6", "MS7",
+                                         "MS8", "MS9", "MS10", "MS11", "MS12",
+                                         "MS13", "MS14", "MS15", "MS16", "MS17", "MS18", 
+                                         "MS19", "MS20", "MS21")), fill = TempCor_F, 
+             y = as.factor(DateTime))) + 
+  geom_tile()+
+  labs(title = "Graphs by Heat for the 10th of Each Month",
+       x = "Sensor Name",
+       y = element_blank())
+
+days_heat_graph
+
+#set up max for cold and warm temperatures
+coldest <- tempdata_filtered %>%
+  filter(TempCor_F == min(TempCor_F))
+
+warmest <- tempdata_filtered %>%
+  filter(TempCor_F == max(TempCor_F))
+
+
+
+
+
+
+
+
+
+
+
+
+#GRAPHS FOR THE 10TH OF EACH MONTH
+tempdata_filtered <- tempdata %>%
   filter(Stream == "MS",
          Type == "Stream",
          Type != "Air",
-         DateTime == chooseDate,
-         TempCor_F < 72) %>%
+         TempCor_F < 72)
+
+#Graph of August 10
+august_10 <- mdy_hms("08-10-2020 12:00:00")
+
+aug_10_graph <- tempdata_filtered %>% 
+  filter(DateTime == august_10) %>%
   ggplot(aes(factor(Name, levels = c("MS1", "MS2", "MS3", "MS4", "MS5", "MS6", "MS7",
                                      "MS8", "MS9", "MS10", "MS11", "MS12",
                                      "MS13", "MS14", "MS15", "MS16", "MS17", "MS18", 
                                      "MS19", "MS20", "MS21")), TempCor_F)) + 
   geom_point()+
-  labs(title = "Water Temp for Each Sensor on 08/08/2020",
+  labs(title = "Water Temp for Each Sensor on 08/10/2020",
        x = "Sensor Name",
        y = "Temperature (F)")
 
-#set up max for cold and warm temperatures, maybe add some graphs for winter solstice, summer solstice, and the equinoxes
+#Graph of september_10
+september_10 <- mdy_hms("09-10-2020 12:00:00")
 
+sep_10_graph <- tempdata_filtered %>% 
+  filter(DateTime == september_10) %>%
+  ggplot(aes(factor(Name, levels = c("MS1", "MS2", "MS3", "MS4", "MS5", "MS6", "MS7",
+                                     "MS8", "MS9", "MS10", "MS11", "MS12",
+                                     "MS13", "MS14", "MS15", "MS16", "MS17", "MS18", 
+                                     "MS19", "MS20", "MS21")), TempCor_F)) + 
+  geom_point()+
+  labs(title = "Water Temp for Each Sensor on 09/10/2020",
+       x = "Sensor Name",
+       y = "Temperature (F)")
+
+#Graph of October 10
+october_10 <- mdy_hms("10-10-2020 12:00:00")
+
+oct_10_graph <- tempdata_filtered %>% 
+  filter(DateTime == october_10) %>%
+  ggplot(aes(factor(Name, levels = c("MS1", "MS2", "MS3", "MS4", "MS5", "MS6", "MS7",
+                                     "MS8", "MS9", "MS10", "MS11", "MS12",
+                                     "MS13", "MS14", "MS15", "MS16", "MS17", "MS18", 
+                                     "MS19", "MS20", "MS21")), TempCor_F)) + 
+  geom_point()+
+  labs(title = "Water Temp for Each Sensor on 10/10/2020",
+       x = "Sensor Name",
+       y = "Temperature (F)")
+
+#Graph of November 10
+november_10 <- mdy_hms("11-10-2020 12:00:00")
+
+nov_10_graph <- tempdata_filtered %>% 
+  filter(DateTime == november_10) %>%
+  ggplot(aes(factor(Name, levels = c("MS1", "MS2", "MS3", "MS4", "MS5", "MS6", "MS7",
+                                     "MS8", "MS9", "MS10", "MS11", "MS12",
+                                     "MS13", "MS14", "MS15", "MS16", "MS17", "MS18", 
+                                     "MS19", "MS20", "MS21")), TempCor_F)) + 
+  geom_point()+
+  labs(title = "Water Temp for Each Sensor on 11/10/2020",
+       x = "Sensor Name",
+       y = "Temperature (F)")
+
+#Graph of December 10
+december_10 <- mdy_hms("12-10-2020 12:00:00")
+
+dec_10_graph <- tempdata_filtered %>% 
+  filter(DateTime == december_10) %>%
+  ggplot(aes(factor(Name, levels = c("MS1", "MS2", "MS3", "MS4", "MS5", "MS6", "MS7",
+                                     "MS8", "MS9", "MS10", "MS11", "MS12",
+                                     "MS13", "MS14", "MS15", "MS16", "MS17", "MS18", 
+                                     "MS19", "MS20", "MS21")), TempCor_F)) + 
+  geom_point()+
+  labs(title = "Water Temp for Each Sensor on 12/10/2020",
+       x = "Sensor Name",
+       y = "Temperature (F)")
+
+#Graph of January 10
+january_10 <- mdy_hms("01-10-2021 12:00:00")
+
+jan_10_graph <- tempdata_filtered %>% 
+  filter(DateTime == january_10) %>%
+  ggplot(aes(factor(Name, levels = c("MS1", "MS2", "MS3", "MS4", "MS5", "MS6", "MS7",
+                                     "MS8", "MS9", "MS10", "MS11", "MS12",
+                                     "MS13", "MS14", "MS15", "MS16", "MS17", "MS18", 
+                                     "MS19", "MS20", "MS21")), TempCor_F)) + 
+  geom_point()+
+  labs(title = "Water Temp for Each Sensor on 01/10/2021",
+       x = "Sensor Name",
+       y = "Temperature (F)")
+
+
+
+#testing out patchwork
+install.packages("patchwork")
+library("patchwork")
+
+aug_10_graph / sep_10_graph / oct_10_graph / nov_10_graph / dec_10_graph / jan_10_graph
+
+
+
+
+
+
+
+
+         
 
 
 
